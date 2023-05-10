dbfc0dc04307a579994dd4445a4f4a5e598da2cd
titawidia <gitawidia166@gmail.com>
2023-05-10 13:54:01 +0700

    First Commit

---
+++ src/main/java/com/itc/pmbsystem/PmbSystemApplication.java
@@ -0,0 +1,13 @@
+package com.itc.pmbsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmbSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmbSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itc/pmbsystem/PmbSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.pmbsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmbSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
